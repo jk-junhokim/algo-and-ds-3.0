47f31ee195fcad58cfc240a2989b9a9c25acd61b
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-11-21 13:25:06 +0000

    gcd adt part 3

--- basic_python/gcd.py
+++ basic_python/gcd.py
@@ -1,4 +1,5 @@
 # gcd function
+# euclid's algorithm
 def gcd(m, n):
     while m % n != 0:
         old_m = m
@@ -14,6 +15,27 @@ class Fraction:
     def __init__(self, top, bottom):
         self.num = top
         self.den = bottom
+        # as you create an instance Fraction(a, b), self.num & self.den are automatically created
 
     def __str__(self):
-        return str
+        return str(self.num) + "/" + str(self.den)
+
+    def show(self):
+        print(self.num, "/", self.den)
+
+    def __add__(self, other_fraction):
+        new_num = self.num * other_fraction.den + self.den * other_fraction.num
+        new_den = self.den * other_fraction.den
+        common = gcd(new_num, new_den)
+        return Fraction(new_num // common, new_den // common)
+
+    def __eq__(self, other):
+        first_num = self.num * other.den
+        second_num = other.num * self.den
+
+        return first_num == second_num
+    
+x = Fraction(1, 2)
+y = Fraction(2, 3)
+print(x + y)
+print(x == y)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 # Programming Exercises Based on "Problem Solving with Algorithms and Data Structures 3.0"
 
-from gcd import gcd
-# import class too???
+from basic_python.gcd import Fraction
+
