98c5bdb1c103e9b980a5563c8c415884f66f2d56
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-12-02 13:47:40 +0000

    search and sort practice part 2

--- basic_python/searchAndSort/search_sort_practice.py
+++ basic_python/searchAndSort/search_sort_practice.py
@@ -15,17 +15,24 @@ def sequential_search_practice(a_list, item):
 # print(sequential_search_practice(test_list, 13))
 # print(sequential_search_practice(test_list, 3))
 
-def binary_search_practice(a_list):
+def binary_search_practice(a_list, item):
+    if len(a_list) == 0:
+        return False
+    else:
+        pos = len(a_list) // 2
 
-    found = False
+    if a_list[pos] == item:
+        return True
+    else:
+        if a_list[pos] < item:
+            return binary_search_practice(a_list[pos + 1:], item)
+        else:
+            return binary_search_practice(a_list[:pos], item)
 
 
-    return found
-
-
-test_list = [1, 2, 32, 8, 17, 19, 42, 13, 0]
-print(binary_search_practice(test_list, 13))
-print(binary_search_practice(test_list, 3))
+# test_list = [1, 2, 3, 8, 17, 19, 24, 155, 200]
+# print(binary_search_practice(test_list, 2))
+# print(binary_search_practice(test_list, 88))
 
 def bubble_sort_practice(a_list):
     pass
