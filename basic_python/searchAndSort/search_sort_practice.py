86daeb78d0b973ae0e8e6224785883faeee66872
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-12-06 14:33:20 +0000

    bubble sort(short) practice done!

--- basic_python/searchAndSort/search_sort_practice.py
+++ basic_python/searchAndSort/search_sort_practice.py
@@ -43,9 +43,23 @@ def bubble_sort_practice(a_list):
                 a_list[i + 1] = temp
     
 
-a_list = [1, 5, 2, 60, 22, 31, 41, 17]
-bubble_sort_practice(a_list)
-print(a_list)
+# a_list = [1, 5, 2, 60, 22, 31, 41, 17]
+# bubble_sort_practice(a_list)
+# print(a_list)
 
 def short_bubble_sort_practice(a_list):
-    pass
+    exchanges = True
+    pass_num = len(a_list) - 1
+    while pass_num > 0 and exchanges:
+        exchanges = False
+        for i in range(pass_num):
+            if a_list[i] > a_list[i + 1]:
+                temp = a_list[i]
+                a_list[i] = a_list[i + 1]
+                a_list[i + 1] = temp
+                exchanges = True
+        pass_num = pass_num - 1
+
+a_list = [1, 5, 2, 60, 22, 31, 41, 17]
+short_bubble_sort_practice(a_list)
+print(a_list)  
